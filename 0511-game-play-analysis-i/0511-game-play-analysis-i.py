5ea17153f2553263126fe31df6fc34507927adec
Arjun <pundirarjun400@gmail.com>
2026-07-08 22:10:07 +0530

    Time: 379 ms (5.01%), Space: 68.1 MB (46.55%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.py
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.py
@@ -1,18 +1,11 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    result = []
-    
-
-    for player in activity["player_id"].unique():
-        temp = activity[activity["player_id"] == player]
-        first = temp.loc[temp["event_date"].idxmin()]
-        result.append({
-            "player_id": first["player_id"],
-            "first_login": first["event_date"]
-        })
-
-    return pd.DataFrame(result)
+    return (
+        activity.groupby("player_id", as_index=False)["event_date"]
+        .min()
+        .rename(columns={"event_date": "first_login"})
+    )
 
 
 
