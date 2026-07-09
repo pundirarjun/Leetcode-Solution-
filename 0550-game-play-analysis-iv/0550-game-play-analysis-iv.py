61f235864d5173939153106eee9308a1bd0fc108
Arjun <pundirarjun400@gmail.com>
2026-07-09 21:39:18 +0530

    Update 0550-game-play-analysis-iv.py

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.py
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.py
@@ -1,7 +1,23 @@
 import pandas as pd
-
+# Second submission
 def gameplay_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     activity["first"] = activity.groupby("player_id")["event_date"].transform(min)
     activity_second = activity.loc[activity["first"] + pd.DateOffset(1) == activity["event_date"]]
     return pd.DataFrame({"fraction" : [round(len(activity_second)/activity["player_id"].nunique(),2)]})
+
+# First submission
+    # activity['event_date'] = pd.to_datetime(activity['event_date'])
+
+    # activity['first_login'] = activity.groupby('player_id')['event_date'].transform('min')
+
+    # next_day_login = activity[
+    #     activity['event_date'] == activity['first_login'] + pd.DateOffset(days=1)
+    # ]
+
+    # total_players = activity['player_id'].nunique()
+    # next_day_players = next_day_login['player_id'].nunique()
+
+    # fraction = next_day_players / total_players if total_players > 0 else 0.0
+
+    # return pd.DataFrame({'fraction': [round(fraction, 2)]})
     
